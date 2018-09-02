d8cc9d963708669e5b56c148d2ae1840c83f9d1c
Maya Vera <maya@mayavera.me>
2018-09-02 16:18:40 -0400

    Now using brew bundle

--- setup.sh
+++ setup.sh
@@ -1,31 +1,9 @@
 #!/usr/bin/env bash
 # (c) Copyright 2018 Maya Vera (mayavera.me)
+if [ -x "$(command -v brew)" ]; then
+  echo Homebrew already installed
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-brew install \
-  awscli \
-  base64 \
-  cmake \
-  dep \
-  ffmpeg \
-  gnupg \
-  go \
-  imagemagick \
-  nginx \
-  node \
-  postgresql \
-  protobuf \
-  python \
-  sqlite \
-  transmission \
-  watch \
-  wget \
-  wput \
-  youtube-dl
-
-brew cask install \
-  google-chrome \
-  google-cloud-sdk \
-  ngrok \
-  transmission \
-  vagrant \
-  virtualbox
+brew bundle
