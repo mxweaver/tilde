8150a42db2663bfd25225e8d5fd7e5832da5df96
Maya Vera <maya@mayavera.me>
2018-09-02 15:20:48 -0400

    Added setup script

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 #!/usr/bin/make -f
 # (c) Copyright 2018 Maya Vera (mayavera.me)
 
-default:
+.PHONY: build setup
+default: build
+
+setup:
+	@./setup.sh
+
+build:
 	@( cd Public && make )
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# (c) Copyright 2018 Maya Vera (mayavera.me)
+
+brew install \
+  awscli \
+  base64 \
+  cmake \
+  dep \
+  ffmpeg \
+  gnupg \
+  go \
+  imagemagick \
+  nginx \
+  node \
+  postgresql \
+  protobuf \
+  python \
+  sqlite \
+  transmission \
+  watch \
+  wget \
+  wput \
+  youtube-dl
+
+brew cask install \
+  transmission \
+  vagrant \
+  virtualbox
